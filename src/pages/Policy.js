4741e11f6119c5a75f1b79a893ad2887d0049372
zoom2009 <afumagic3@gmail.com>
2020-06-10 13:22:52 +0700

    Add favicon & policyPage

---
+++ src/pages/Policy.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Policy = () => {
+  return (
+    <div style={{ padding: '0 20px' }}>
+      <h1>Welcome to our Privacy Policy</h1>
+      <h3>Your privacy is critically important to us.</h3>
+			Privacy Policy is located at:<br />
+      <address>
+        Privacy Policy<br />25 Udomsuk Room 401, Huikwang <br />10310 - Bangkok , Thailand<br />66935761336
+      </address>
+
+      <p>It is Privacy Policy's policy to respect your privacy regarding any information we may collect while operating our website. This Privacy Policy applies to <a href="https://smart-postor.com">https://smart-postor.com</a> (hereinafter, "us", "we", or "https://smart-postor.com"). We respect your privacy and are committed to protecting personally identifiable information you may provide us through the Website. We have adopted this privacy policy ("Privacy Policy") to explain what information may be collected on our Website, how we use this information, and under what circumstances we may disclose the information to third parties. This Privacy Policy applies only to information we collect through the Website and does not apply to our collection of information from other sources.</p>
+      <p>This Privacy Policy, together with the Terms and conditions posted on our Website, set forth the general rules and policies governing your use of our Website. Depending on your activities when visiting our Website, you may be required to agree to additional terms and conditions.</p>
+
+      <h2>Website Visitors</h2>
+      <p>Like most website operators, Privacy Policy collects non-personally-identifying information of the sort that web browsers and servers typically make available, such as the browser type, language preference, referring site, and the date and time of each visitor request. Privacy Policy's purpose in collecting non-personally identifying information is to better understand how Privacy Policy's visitors use its website. From time to time, Privacy Policy may release non-personally-identifying information in the aggregate, e.g., by publishing a report on trends in the usage of its website.</p>
+      <p>Privacy Policy also collects potentially personally-identifying information like Internet Protocol (IP) addresses for logged in users and for users leaving comments on https://smart-postor.com blog posts. Privacy Policy only discloses logged in user and commenter IP addresses under the same circumstances that it uses and discloses personally-identifying information as described below.</p>
+
+      <h2>Gathering of Personally-Identifying Information</h2>
+      <p>Certain visitors to Privacy Policy's websites choose to interact with Privacy Policy in ways that require Privacy Policy to gather personally-identifying information. The amount and type of information that Privacy Policy gathers depends on the nature of the interaction. For example, we ask visitors who sign up for a blog at https://smart-postor.com to provide a username and email address.</p>
+
+      <h2>smart-postor</h2>
+      <p>smart-postor</p>
+
+      <h2>Credit & Contact Information</h2>
+      <p>This privacy policy was created at <a style={{ color: 'inherit', textDecoration: 'none' }} href="https://termsandconditionstemplate.com/privacy-policy-generator/" title="Privacy policy template generator" target="_blank">termsandconditionstemplate.com</a>. If you have any questions about this Privacy Policy, please contact us via <a href="mailto:smart-postor@gmail.com">email</a> or <a href="tel:66935761336">phone</a>.</p>
+    </div>
+  )
+}
+
+export default Policy
