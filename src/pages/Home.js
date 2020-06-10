a08820b17a8aa5c85a72742ca90fb5fb75154060
zoom2009 <afumagic3@gmail.com>
2020-06-10 13:28:56 +0700

    add button to move policy

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <div>
-      Home Page
+      Home Page<br /><br />
+      <Link to="/policy">Policy Page</Link>
     </div>
   )
 }
